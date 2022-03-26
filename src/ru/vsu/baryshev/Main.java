02614e80c88650f3c07c951de252daa45d0e8c30
Wasilewski6860 <trollivanovich6860@gmail.com>
2022-03-26 19:57:42 +0300

    sort fixed

--- src/ru/vsu/baryshev/Main.java
+++ src/ru/vsu/baryshev/Main.java
@@ -6,27 +6,21 @@ public class Main {
 
 
     public static <T> T[] selectionSort(T[] data,boolean[] bArr,Comparator<T> c){
-    /*По очереди будем просматривать все подмножества
-      элементов массива (0 - последний, 1-последний,
-      2-последний,...)*/
         for (int i = 0; i < data.length; i++) {
 
             if (bArr[i]) {
-        /*Предполагаем, что первый элемент (в каждом
-           подмножестве элементов) является минимальным */
+
                 T min = data[i];
                 int min_i = i;
-        /*В оставшейся части подмножества ищем элемент,
-           который меньше предположенного минимума*/
+
                 for (int j = i + 1; j < data.length; j++) {
-                    //Если находим, запоминаем его индекс
+
                     if (c.compare(data[j], min) < 0 && bArr[j]) {
                         min = data[j];
                         min_i = j;
                     }
                 }
-        /*Если нашелся элемент, меньший, чем на текущей позиции,
-          меняем их местами*/
+
                 if (i != min_i) {
                     T tmp = data[i];
                     data[i] = data[min_i];
@@ -39,15 +33,25 @@ public class Main {
     }
 
     public static <T extends Comparable<T>> void sort(T[] data,boolean[] bArr) {
-        selectionSort(data,bArr, Comparable::compareTo);
+
+        class TempComparator implements Comparator<T> {
+            @Override
+            public int compare(T a, T b) {
+                return a.compareTo(b);
+            }
+        }
+
+        selectionSort(data,bArr, new TempComparator());
     }
 
 
     public static void main(String[] args) {
 
-        int[] array = {7, 10, 3, 8, 7, 2, 1, 9, 5, 7 };
+        Integer[] array = {7, 10, 3, 8, 7, 2, 1, 9, 5, 7 };
         boolean[] booleans ={true,false,true,true,false,false,true,true,false,true};
-        array=sort(array,booleans);
+
+        sort(array,booleans);
+
         System.out.println();
         for (int i=0;i< array.length;i++){
             System.out.print(array[i]+" ");
